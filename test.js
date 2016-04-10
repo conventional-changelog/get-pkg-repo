e4d8d214e3a5f7ac33a065355e658844f46f1fd7
Thiago Felix <thiago@thiagofelix.com>
2016-04-10 11:41:37 -0300

    Update tests to cover user/project/commit attributes

--- test.js
+++ test.js
@@ -7,63 +7,90 @@ var parse = function(url, fix) {
   }, fix);
 };
 
+var assertRepo = function(repo, expected) {
+  assert.equal(repo.browse(), expected.browse);
+  assert.equal(repo.type, expected.type);
+  assert.equal(repo.user, expected.user);
+  assert.equal(repo.project, expected.project);
+  assert.equal(repo.committish, expected.committish);
+};
+
 it('should parse github http', function() {
   var repo = parse('http://github.com/a/b');
-
-  assert.equal(repo.browse(), 'https://github.com/a/b');
-  assert.equal(repo.type, 'github');
+  assertRepo(repo, {
+    browse: 'https://github.com/a/b',
+    type: 'github',
+    user: 'a',
+    project: 'b'
+  });
 });
 
 it('should parse github https', function() {
   var repo = parse('https://github.com/a/b');
-
-  assert.equal(repo.browse(), 'https://github.com/a/b');
-  assert.equal(repo.type, 'github');
+  assertRepo(repo, {
+    browse: 'https://github.com/a/b',
+    type: 'github',
+    user: 'a',
+    project: 'b'
+  });
 });
 
 it('should parse github ssh', function() {
   var repo = parse('git@github.com:joyent/node.git');
-
-  assert.equal(repo.browse(), 'https://github.com/joyent/node');
-  assert.equal(repo.type, 'github');
+  assertRepo(repo, {
+    browse: 'https://github.com/joyent/node',
+    type: 'github',
+    user: 'joyent',
+    project: 'node'
+  });
 });
 
 it('should parse github short', function() {
   var repo = parse('a/b');
-
-  assert.equal(repo.browse(), 'https://github.com/a/b');
-  assert.equal(repo.type, 'github');
+  assertRepo(repo, {
+    browse: 'https://github.com/a/b',
+    type: 'github',
+    user: 'a',
+    project: 'b'
+  });
 });
 
 it('should parse bitbucket', function() {
   var repo = parse('https://bitbucket.org/a/b.git');
-
-  assert.equal(repo.type, 'bitbucket');
-  assert.equal(repo.browse(), 'https://bitbucket.org/a/b');
+  assertRepo(repo, {
+    browse: 'https://bitbucket.org/a/b',
+    type: 'bitbucket',
+    user: 'a',
+    project: 'b'
+  });
 });
 
 it('should parse svn', function() {
   var repo = parse('svn://a/b');
-
-  assert.equal(repo.browse(), 'http://a/b');
+  assertRepo(repo, {
+    browse: 'http://a/b'
+  });
 });
 
 it('should parse https', function() {
   var repo = parse('https://a/b');
-
-  assert.equal(repo.browse(), 'https://a/b');
+  assertRepo(repo, {
+    browse: 'https://a/b'
+  });
 });
 
 it('should parse a url with an @', function() {
   var repo = parse('a@b.com');
-
-  assert.equal(repo.browse(), 'http://b.com');
+  assertRepo(repo, {
+    browse: 'http://b.com'
+  });
 });
 
 it('should fix bad protocal', function() {
   var repo = parse('badprotocol://a/b');
-
-  assert.equal(repo.browse(), 'http://a/b');
+  assertRepo(repo, {
+    browse: 'http://a/b'
+  });
 });
 
 it('should work with a json', function() {
@@ -73,16 +100,22 @@ it('should work with a json', function() {
     }
   });
   var repo = getPkgRepo(jsonData);
-
-  assert.equal(repo.browse(), 'https://github.com/a/b');
-  assert.equal(repo.type, 'github');
+  assertRepo(repo, {
+    browse: 'https://github.com/a/b',
+    type: 'github',
+    user: 'a',
+    project: 'b'
+  });
 });
 
 it('should work if there is a typo', function() {
   var repo = getPkgRepo({repo: 'a/b'}, true);
-
-  assert.equal(repo.browse(), 'https://github.com/a/b');
-  assert.equal(repo.type, 'github');
+  assertRepo(repo, {
+    browse: 'https://github.com/a/b',
+    type: 'github',
+    user: 'a',
+    project: 'b'
+  });
 });
 
 it('should error if cannot get repository', function() {
@@ -92,7 +125,12 @@ it('should error if cannot get repository', function() {
 });
 
 it('should parse github enterprise http url', function() {
-  var url = 'http://github.mycompany.dev/user/myRepo';
-  assert.equal(parse(url).browse(), 'http://github.mycompany.dev/user/myRepo');
-  assert.equal(parse(url).user, 'user');
+  var url = 'http://github.mycompany.dev/user/myRepo#123';
+  var repo = parse(url);
+  assertRepo(repo, {
+    browse: 'http://github.mycompany.dev/user/myRepo',
+    user: 'user',
+    project: 'myRepo',
+    committish: '123',
+  });
 });
