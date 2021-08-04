8017a91f7de2a3dbfcb627ce51c3ca5ce8e2c172
githrdw <1455140+githrdw@users.noreply.github.com>
2021-08-04 04:54:16 +0200

    feat(cli): use Yargs in stead of meow (#62)
    Co-authored-by: Ruben de Wit <ruben@acato.nl>

--- src/cli.js
+++ src/cli.js
@@ -4,34 +4,28 @@
 
 const fs = require(`fs`);
 const getPkgRepo = require(`../`);
-const meow = require(`meow`);
 const through = require(`through2`);
 const util = require(`util`);
 
-const cli = meow({
-  help:
-    `Practice writing repository URL or validate the repository in a package.json file.
-    If used without specifying a package.json file path, you will enter an interactive shell.
-    Otherwise, the repository info in package.json is printed.
+const yargs = require('yargs/yargs')(process.argv.slice(2))
+  .usage(
+    '\nPractice writing repository URL or validate the repository in a package.json file. If used without specifying a package.json file path, you will enter an interactive shell. Otherwise, the repository info in package.json is printed.'
+  )
+  .scriptName('get-pkg-repo')
+  .command('$0')
+  .command('<path> [<path> ...]')
+  .example('get-pkg-repo')
+  .example('get-pkg-repo package.json')
+  .example('cat package.json | get-pkg-repo')
+  .help().argv;
 
-    Usage
-      get-pkg-repo
-      get-pkg-repo <path> [<path> ...]
-      cat <path> | get-pkg-repo
-
-    Examples
-      get-pkg-repo
-      get-pkg-repo package.json
-      cat package.json | get-pkg-repo`,
-});
-
-const {input} = cli;
+const input = yargs._;
 
 if (process.stdin.isTTY) {
   if (input.length > 0) {
     input.forEach(path => {
       let repo;
-      fs.readFile(path, `utf8`, (err, data) => {
+      fs.readFile(path, 'utf8', (err, data) => {
         if (err) {
           console.error(err);
           return;
@@ -47,33 +41,37 @@ if (process.stdin.isTTY) {
     });
   } else {
     process.stdin
-      .pipe(through.obj((chunk, enc, cb) => {
-        let repo;
-        const pkgData = {
-          repository: chunk.toString(),
-        };
+      .pipe(
+        through.obj((chunk, enc, cb) => {
+          let repo;
+          const pkgData = {
+            repository: chunk.toString(),
+          };
 
-        try {
-          repo = getPkgRepo(pkgData);
-          cb(null, util.format(repo) + `\n`);
-        } catch (e) {
-          console.error(e.toString());
-          cb();
-        }
-      }))
+          try {
+            repo = getPkgRepo(pkgData);
+            cb(null, util.format(repo) + '\n');
+          } catch (e) {
+            console.error(e.toString());
+            cb();
+          }
+        })
+      )
       .pipe(process.stdout);
   }
 } else {
   process.stdin
-    .pipe(through.obj((chunk, enc, cb) => {
-      let repo;
-      try {
-        repo = getPkgRepo(JSON.parse(chunk.toString()));
-      } catch (e) {
-        console.error(e.toString());
-        process.exit(1);
-      }
-      cb(null, `${util.format(repo)}\n`);
-    }))
+    .pipe(
+      through.obj((chunk, enc, cb) => {
+        let repo;
+        try {
+          repo = getPkgRepo(JSON.parse(chunk.toString()));
+        } catch (e) {
+          console.error(e.toString());
+          process.exit(1);
+        }
+        cb(null, util.format(repo) + '\n');
+      })
+    )
     .pipe(process.stdout);
 }
